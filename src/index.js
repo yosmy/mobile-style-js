4800304907c2717773fc45e0a18b7b4d438e89ae
Yosmany Garcia <yosmanyga@gmail.com>
2020-05-29 19:41:49 -0700

    Added first draft

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import styled, {ThemeProvider, withTheme} from 'styled-components';
+import match from './match';
+
+export {
+    styled,
+    ThemeProvider,
+    withTheme,
+    match
+}
---
+++ src/match.js
@@ -0,0 +1,15 @@
+const match = (elements, string) => {
+    if (!string) {
+        return false;
+    }
+
+    for (let i = 0; i <= elements.length - 1; i++) {
+        if (!string.includes(elements[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+export default match
